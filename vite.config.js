2ad02460f830bbc9434d87ace4bd9b3705222fbd
yadavnan <yadavnan55@gmail.com>
2025-01-02 15:37:03 +0530

    repuupdt

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/memory-game/',
+  base: '/memory-game/', // This must match your GitHub repository name
 });
+
